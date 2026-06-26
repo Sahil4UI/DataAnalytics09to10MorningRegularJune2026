a1dd8a4ca8b247c196f9798044a8f82b0061df80
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-06-26 09:46:39 +0530

    Add files via upload

---
+++ input and output.py
@@ -0,0 +1,14 @@
+x = 2
+y = 5
+print("The Sum of",x,"and",y,"is",x+y)
+print("The Sum of %d and %d is %d"%(x,y,x+y))
+print("The Sum of {} and {} is {}".format(x,y,x+y))
+print(f"The Sum of {x} and {y} is {x+y}")
+
+
+a=856
+b=524
+print("The sum OF",a,"and",b,"is",a+b)
+print("The sum of %d and %d is %d"%(a,b,a+b))
+print("The sum of {} and {} is {}".format(a,b,a+b))
+print(f"The sum of {a} and {b} is {a+b}")
