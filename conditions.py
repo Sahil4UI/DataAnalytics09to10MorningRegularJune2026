b0625e6a34adb3e055d731860bad618433de16f9
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-06-27 09:49:06 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,37 @@
+#write a program to check whether a given no is even or odd
+'''
+x = int(input("Enter no : "))
+if x%2==0:
+    print("Even No")
+else:
+    print("Odd No")
+'''
+#write a program to find the largest b/w two nos
+'''
+x = int(input("Enter no1 : "))
+y = int(input("Enter no2 : "))
+
+if x>y:
+    print(f"{x} is largest")
+else:
+    print(f"{y} is largest")
+'''
+
+#write a program to find the largest b/w three nos
+'''
+x = int(input("Enter no1 : "))
+y = int(input("Enter no2 : "))
+z = int(input("Enter no3 : "))
+
+if x>y and x>z:
+    print(x,"is largest")
+elif y>z:
+    print(y,"is largest")
+else:
+    print(z,"is largest")
+'''
+
+#take 3 sides as input, and if they form a traingle,state the type of traingle : 
+# equilateral
+# Isoceles
+# Scalene
